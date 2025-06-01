24d4243a1a49d6798549229f61b5e1dd113821d5
Terrance Li <terranceli.terbear@gmail.com>
2025-06-01 14:42:12 -0700

    Support padding, default 1:1 ratio

--- main.py
+++ main.py
@@ -12,10 +12,13 @@ def parse_ratio_str(ratio_str: str):
   [aspect_ratio_width, aspect_ratio_height] = ratio_str.split(":")
   return float(aspect_ratio_width), float(aspect_ratio_height)
 
-def compute_padded_size(old_size, aspect_ratio_width, aspect_ratio_height):
+def compute_padded_size(old_size, aspect_ratio_width, aspect_ratio_height, padding):
   width = int(max(old_size[1] / aspect_ratio_height * aspect_ratio_width, old_size[0]))
   height = int(max(old_size[0] / aspect_ratio_width * aspect_ratio_height, old_size[1]))
-  return width, height
+
+  width /= (1-padding)
+  height /= (1-padding)
+  return int(width), int(height)
 
 def get_output_filepath(input_filepath: str, aspect_ratio_width, aspect_ratio_height):
   filename, ext = os.path.splitext(input_filepath)
@@ -28,7 +31,7 @@ def main(args):
   
   # Image .size's are in (width, height) format
   old_size = im.size
-  new_size = compute_padded_size(old_size, aspect_ratio_width, aspect_ratio_height)
+  new_size = compute_padded_size(old_size, aspect_ratio_width, aspect_ratio_height, args.padding)
   
   # create a new image and paste the resized on it
   new_im = Image.new("RGB", new_size, COLOR_MAP[args.color])
@@ -41,7 +44,8 @@ def main(args):
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument('path', help='Path to the image file. Only JPGs are supported.')
-  parser.add_argument('ratio', help='Aspect ratio, in "width:height" format, e.g. "16:9".')
+  parser.add_argument('--ratio', required=False, default="1:1", help='Aspect ratio, in "width:height" format, e.g. "16:9".')
   parser.add_argument('--color', required=False, default="white", help='Color to pad with. "white" (default) or "black".')
+  parser.add_argument('--padding', required=False, default=0.0, type=float, help='Proportion of padding to image for the output image, e.g. 0.15')
   args = parser.parse_args()
   main(args)
